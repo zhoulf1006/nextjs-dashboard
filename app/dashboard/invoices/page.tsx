c8c0023d9642684e51d5a12da7989911bf8558df
loong_zhou <loong_zhou@trendmicro.com>
2024-10-08 11:24:58 +0800

    feat: chapter6 deployment

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-4xl">Invoices Page</h1>
+    </div>
+  )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-4xl">Dashboard Page</h1>
+    </div>
+  )
+}
